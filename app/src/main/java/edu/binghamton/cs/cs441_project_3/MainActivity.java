d0f72ccc0deba7529b02ea9507ae789b54d3be46
knadeem1 <35601623+knadeem1@users.noreply.github.com>
2019-10-01 01:02:22 -0400

    features added

--- app/src/main/java/edu/binghamton/cs/cs441_project_3/MainActivity.java
+++ app/src/main/java/edu/binghamton/cs/cs441_project_3/MainActivity.java
@@ -3,12 +3,17 @@ package edu.binghamton.cs.cs441_project_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(new GameView(this));
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
     }
 }
---
+++ app/src/main/java/edu/binghamton/cs/cs441_project_3/MainThread.java
@@ -0,0 +1,10 @@
+package edu.binghamton.cs.cs441_project_3;
+
+import android.view.SurfaceHolder;
+
+public class MainThread extends Thread {
+
+    public MainThread(SurfaceHolder surrfaceHolder, GameView gameView){
+        super();
+    }
+}
