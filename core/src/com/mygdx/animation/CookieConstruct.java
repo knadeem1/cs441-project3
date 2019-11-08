2791d766aff7c3711f8a261f69598c511f8be641
knadeem1 <35601623+knadeem1@users.noreply.github.com>
2019-11-08 10:29:31 -0500

    updates 1.0

--- core/src/com/mygdx/animation/CookieConstruct.java
+++ core/src/com/mygdx/animation/CookieConstruct.java
@@ -4,9 +4,118 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import sun.applet.Main;
+
 public class CookieConstruct extends Game {
     SpriteBatch batch;
     BitmapFont font;
+
+    SpriteBatch batch2;
+    BitmapFont font2;
+    SpriteBatch batch3;
+    BitmapFont font3;
+    SpriteBatch batch4;
+    BitmapFont font4;
+    SpriteBatch batch5;
+    BitmapFont font5;
+
+    public void create(){
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this,0,0));
+        public void create(){
+            batch = new SpriteBatch();
+            font = new BitmapFOnt();
+            this.setScreen(new MainMenuScreen(this,0,0));
+            public void create(){
+                batch = new SpriteBatch();
+                font = new BitmapFont();
+                this.setScreen(new MainMenuScreen((this,0,0));
+                public void create(){
+                    batch = new SpriteBatch();
+                    font = new BitmapFont();
+                    this.setScreen(new MainMenuScreen(this,0,0));
+                    public void create(){
+                        batch = new SpriteBatch();
+                        font = new BitmapFont();
+                        this.setScreen(new MainMenuScreen(this,0,0));
+                        public void create(){
+                            batch = new SpriteBatch();
+                            font = new BitmapFont();
+                            this.setScreen(new MainMenuScreen(this,0,0));
+                            public void create(){
+                                batch = new SpriteBatch();
+                                font = new BitmapFont();
+                                this.setScreen(new MainMenuScreen(this,0,0));
+                                public void create(){
+                                    batch = new SpriteBatch();
+                                    font = new BitmapFont();
+                                    this.setScreen(new MainMenuScreen(this,0,0));
+                                    public void create(){
+                                        batch = new SpriteBatch();
+                                        font = new BitmapFont();
+                                        this.setScreen(new MainMenuScreen(this,0,0));
+                                        public void create(){
+                                            batch = new SpriteBatch();
+                                            font = new BitmapFont();
+                                            this.setScreen(new MainMenuScreen(this,0,0));
+                                            public void create(){
+                                                batch = new SpriteBatch();
+                                                font = new BitmapFont();
+                                                this.setScreen(new MainMenuScreen(this,0,0));
+                                            }
+                                            public void render() {
+                                                super.render(); // important!
+                                            }
+                                        }
+                                        public void render(){
+                                            super.render();
+                                        }
+                                        public void render(){
+                                            super.render();
+                                        }
+                                        public void dispose() {
+                                            batch.dispose();
+                                            font.dispose();
+                                        }
+                                        public void dispose(){
+                                            batch.dispose();
+                                            font.dispose();
+                                        }
+                                        public void dispose(){
+                                            batch.dispose();
+                                            font.dispose();
+                                        }
+                                        public void dispose(){
+                                            batch.dispose();
+                                            font.dispose();
+                                        }
+                                        public void dispose(){
+                                            batch.dispose();
+                                            font.dispose();
+                                        }
+                                        public void create(){
+                                            batch = new SpriteBatch();
+                                            font = new BitmapFont();
+                                            this.setScreen(new MainMenuScreen(this,0,0));
+                                            public void dispose(){
+                                                batch.dispose();
+                                                font.dispose();
+                                                this.setScreen(new MainMenuScreen(this,0,0));
+                                                public void render(){
+                                                    super.render();
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     public void create() {
         batch = new SpriteBatch();
